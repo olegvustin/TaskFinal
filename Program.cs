7e12ba34384e561676d711ab7643c3f57290e08c
oleg vustin <olegvustin@gmail.com>
2023-03-08 22:29:16 +0300

    Сделал программу для решения задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося
+массива строк формирует массив из строк,
+длина которых <=3 символа. Первоначальный маcсив можно задать на старте выполнения алгоритма.*/
+
+string[] array = { "hello", "2", "world", ":-)" , "1234", "1567", "-2", "computer science"};
+
+void MethodCreateInputArray(string[] InputArray)
+{
+    Console.Write($"Input array: ");
+    for (int i = 0; i < InputArray.Length; i++) Console.Write($"{InputArray[i]} ");
+    Console.WriteLine();
+}
+
+void MethodCreateOutputArray(string[] OutputArray)
+{
+    Console.Write($"Output array: ");
+    string[] newarr = new string[OutputArray.Length];
+    for (int i = 0; i < OutputArray.Length; i++)
+    {
+        if (OutputArray[i].Length <= 3)
+        {
+            newarr[i] = OutputArray[i];
+            Console.Write($"{newarr[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine();
+MethodCreateInputArray(array);
+Console.WriteLine();
+MethodCreateOutputArray(array);
+Console.WriteLine();
